f5b8f54d01e73059c7c88181ea9156842f5d3ebc
Phil <phildev013@gmail.com>
2022-01-21 13:27:17 +0100

    create CRUD_path back

--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,37 @@ router.get('/weather', function(req,res,next){
   res.render('weather', {cityList})
 })
 
+/*---- AddCity -----*/
+router.post('/add-city', function(req,res,next){
+
+  var alreadyExist = false;
+
+  for(var i=0; i<cityList.length;i++){
+    if(req.body.newcity == cityList[i].name){
+      alreadyExist= true;
+    }
+  }
+
+  if(alreadyExist == false){
+    cityList.push({
+      name:req.body.newcity,
+      desc: "couvert",
+      img: "/images/picto-1.png",
+      temp_max: 1,
+      temp_min: -2
+    })
+  }
+
+
+  res.render('weather', {cityList})
+})
+
+/*---- delete city ------*/
+router.get('/delete-city', function(req,res,next){
+  cityList.splice(req.query.position, 1)
+  res.render('weather', {cityList})
+})
+
+
+
 module.exports = router;
